835366b4d4d8bda47439ce1286eafb649cbeb7fe
Emilis Kiškis <emilis.kiskis@mif.stud.vu.lt>
2019-10-20 22:44:31 +0300

    Added basic chat hub

---
+++ StudyBuddyBackend/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace StudyBuddyBackend.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task SendMessage(string user, string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
