93b2a29eb84eb19139a6176f75d6a2951cf5ae80
Emilis Kiškis <emilis.kiskis@mif.stud.vu.lt>
2019-12-07 16:48:45 +0200

    Added Microsoft validation to User entity

--- StudyBuddyBackend/Database/Entities/User.cs
+++ StudyBuddyBackend/Database/Entities/User.cs
@@ -8,28 +8,35 @@ namespace StudyBuddyBackend.Database.Entities
     public class User
     {
         [Column(TypeName = "VARCHAR")]
-        [StringLength(255)]
+        [StringLength(255, MinimumLength = 1)]
+        [Required]
         [Key]
         public string Username { get; set; }
 
         [Column(TypeName = "VARCHAR")]
         [StringLength(255)]
+        [Required]
         public string Password { get; set; }
 
         [Column(TypeName = "VARCHAR")]
         [StringLength(255)]
+        [Required]
         public string Salt { get; set; }
 
         [Column(TypeName = "VARCHAR")]
         [StringLength(255)]
+        [Required]
         public string FirstName { get; set; }
 
         [Column(TypeName = "VARCHAR")]
         [StringLength(255)]
+        [Required]
         public string LastName { get; set; }
 
         [Column(TypeName = "VARCHAR")]
         [StringLength(255)]
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         // Initializing lists because empty sets in database create null values
